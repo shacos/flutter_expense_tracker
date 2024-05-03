864f691eaca13599667cc38c1f2c791e35611f5f
Mehdi Chikhi <ci_mi@outlook.fr>
2024-05-03 20:56:48 +0100

    finishing lecture 135

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/new_expense.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -102,7 +103,9 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('Chart...'),
+          Chart(
+            expenses: _registeredExpenses,
+          ),
           mainContent,
         ],
       ),
